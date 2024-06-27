c749f7b664d8c35798ca4cee59e933938696db23
euclid <euclid@euclid--MBP.lan>
2024-06-28 00:12:18 +0400

    Modify test example

--- cpp/test_a_star.py
+++ cpp/test_a_star.py
@@ -31,65 +31,66 @@ with open("../assets/tests/test_distance.pickle", "rb") as f:
 # a_star.run_openmp_test()
 
 cpp_a_star.init_envs(actions)
+cpp_a_star.run_openmp_test()
 
-game = Cube3Game("../assets/envs/cube_3_3_3_actions.pickle")
+# game = Cube3Game("../assets/envs/cube_3_3_3_actions.pickle")
 
 # print("Start alloc test")
 # cpp_a_star.test_allocation_dealocation()
 # print("End alloc test")
 
 
-t = 931
-state = test_states[t]
-model = CatBoostRegressor()
-model.load_model("../assets/models/catboost_cube3.cb")
+# t = 931
+# state = test_states[t]
+# model = CatBoostRegressor()
+# model.load_model("../assets/models/catboost_cube3.cb")
 
 
-start_predict = model.predict(state)
-target_predict = 16
-diff_predict = int(start_predict - target_predict)
+# start_predict = model.predict(state)
+# target_predict = 16
+# diff_predict = int(start_predict - target_predict)
 
-start_path = np.random.choice(
-    a=len(game.actions),
-    size=diff_predict
-)
+# start_path = np.random.choice(
+#     a=len(game.actions),
+#     size=diff_predict
+# )
 
-print("start_predict:", start_predict)
-print("diff predict:", diff_predict)
-print("start_path:", start_path)
+# print("start_predict:", start_predict)
+# print("diff predict:", diff_predict)
+# print("start_path:", start_path)
 
-current_predict = start_predict
-current_path = start_path
+# current_predict = start_predict
+# current_path = start_path
 
-while current_predict > target_predict:
-    random_pos = np.random.randint(0, len(current_path))
-    random_action = np.random.randint(0, len(game.actions))
+# while current_predict > target_predict:
+#     random_pos = np.random.randint(0, len(current_path))
+#     random_action = np.random.randint(0, len(game.actions))
 
-    new_path = current_path.copy()
-    new_path[random_pos] = random_action
+#     new_path = current_path.copy()
+#     new_path[random_pos] = random_action
 
-    current_state = state.copy()    
-    for a in current_path:
-        current_state = game.apply_action(state=current_state, action=a)
+#     current_state = state.copy()    
+#     for a in current_path:
+#         current_state = game.apply_action(state=current_state, action=a)
 
-    new_predict = model.predict(current_state)
-    epsilon = np.random.rand()
+#     new_predict = model.predict(current_state)
+#     epsilon = np.random.rand()
     
-    if new_predict < current_predict or epsilon > 0.9:
-        current_predict = new_predict
-        current_path = new_path
-        print("Prediction:", new_predict)
+#     if new_predict < current_predict or epsilon > 0.9:
+#         current_predict = new_predict
+#         current_path = new_path
+#         print("Prediction:", new_predict)
 
-current_state = state.copy()    
-for a in current_path:
-    current_state = game.apply_action(state=current_state, action=a)
+# current_state = state.copy()    
+# for a in current_path:
+#     current_state = game.apply_action(state=current_state, action=a)
 
-result = cpp_a_star.search_a(
-    current_state, # state
-    1_000_000, # limit size
-    True # debug
-)
+# result = cpp_a_star.search_a(
+#     current_state, # state
+#     1_000_000, # limit size
+#     True # debug
+# )
 
-print("Result actions: ", result.actions)
-print("Result h_values: ", [np.round(h, 3) for h in result.h_values])
-print("Result visit_nodes: ", result.visit_nodes)
+# print("Result actions: ", result.actions)
+# print("Result h_values: ", [np.round(h, 3) for h in result.h_values])
+# print("Result visit_nodes: ", result.visit_nodes)
